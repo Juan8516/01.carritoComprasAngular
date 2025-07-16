dcb124075d7bc58725fae85874da16a1f17800fb
Juan8516 <juan.ind85@gmail.com>
2025-07-16 18:44:58 -0500

    estructura carrito

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Carrito } from './components/carrito/carrito';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Carrito],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
