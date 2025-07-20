b12f42ddc55fc74f4fb7bfbf963f6ae2a495f988
Juan8516 <juan.ind85@gmail.com>
2025-07-19 23:47:15 -0500

    funcionalidad agregar productos

--- src/app/components/carrito/carrito.ts
+++ src/app/components/carrito/carrito.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './carrito.html',
   styleUrl: './carrito.css'
 })
 export class Carrito {
 
+  nombreTienda: string = "Mi tienda";
   cantidadProductos: number = 0;
   totalCarrito: number = 0;
   notificacion: string = "";
 
   ngOnInit() {
+    this.comprobarNotificacion();
+  }
+
+  addProducto() {
+    this.cantidadProductos += 1;
+    this.totalCarrito += 100000;
+
+    console.log(`Nuevo producto agregado. precio total ${this.totalCarrito} pesos`);
     this.comprobarNotificacion();
   }
 
